b9fbfd65f17e7410ad7b16dad22c04a00b752455
anish730 <56527626+anish730@users.noreply.github.com>
2022-09-23 21:26:39 +0545

    Day11

---
+++ power_of2.py
@@ -0,0 +1,7 @@
+terms = 10
+# use anonymous function
+result = list(map(lambda x: 2 ** x, range(terms)))
+
+print("The total terms are:",terms)
+for i in range(terms):
+   print("2 raised to power",i,"is",result[i])
